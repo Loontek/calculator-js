8d7cc1bc55c4a002df23bac3c5e3ba328f92ecca
Loontek <sojjkin02@mail.ru>
2022-10-08 18:34:06 +0300

    fix: fix expressions with float numbers

--- src/modules/controller/calc-result.js
+++ src/modules/controller/calc-result.js
@@ -3,19 +3,39 @@ import store from "../model/store";
 export function calcResult() {
 	switch (store.operator) {
 		case '+':
-			store.result = +store.firstNumber + +store.secondNumber;
+			if (store.firstNumber.includes('.') && store.secondNumber.includes('.')) {
+				store.result = (+store.firstNumber * 10000 + +store.secondNumber * 10000) / 10000;
+			} else {
+				store.result = +store.firstNumber + +store.secondNumber;
+			}
 			break;
 		case '-':
-			store.result = +store.firstNumber - +store.secondNumber;
+			if (store.firstNumber.includes('.') && store.secondNumber.includes('.')) {
+				store.result = (+store.firstNumber * 10000 - +store.secondNumber * 10000) / 10000;
+			} else {
+				store.result = +store.firstNumber - +store.secondNumber;
+			}
 			break;
 		case 'x':
-			store.result = +store.firstNumber * +store.secondNumber;
+			if (store.firstNumber.includes('.') && store.secondNumber.includes('.')) {
+				store.result = (+store.firstNumber * 10000) * (+store.secondNumber * 10000) / 100000000;
+			} else {
+				store.result = +store.firstNumber * +store.secondNumber;
+			}
 			break;
 		case '*':
-			store.result = +store.firstNumber * +store.secondNumber;
+			if (store.firstNumber.includes('.') && store.secondNumber.includes('.')) {
+				store.result = (+store.firstNumber * 10000) * (+store.secondNumber * 10000) / 100000000;
+			} else {
+				store.result = +store.firstNumber * +store.secondNumber;
+			}
 			break;
 		case '/':
-			store.result = +store.firstNumber / +store.secondNumber;
+			if (store.firstNumber.includes('.') && store.secondNumber.includes('.')) {
+				store.result = (+store.firstNumber * 10000) / (+store.secondNumber * 10000);
+			} else {
+				store.result = +store.firstNumber / +store.secondNumber;
+			}
 			break;
 		default:
 			store.result;

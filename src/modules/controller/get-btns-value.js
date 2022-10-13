f45c59c3f4b955d02091eacd08e0c4de4ccc5861
Loontek <sojjkin02@mail.ru>
2022-10-13 22:57:06 +0300

    fix: change multi button logic

--- src/modules/controller/get-btns-value.js
+++ src/modules/controller/get-btns-value.js
@@ -5,7 +5,11 @@ import { showInput } from "../view/show-input.js";
 export function getBtnsValue() {
 	for (let btn of elems.keypadBtns) {
 		btn.addEventListener('click', (event) => {
-			store.inputValue += event.target.name;
+			if (event.target.name === 'x') {
+				store.inputValue += '*';
+			} else {
+				store.inputValue += event.target.name;
+			}
 			showInput();
 		});
 	};
